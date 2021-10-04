5768abe825cf869840bb72709bbc140f7116bde2
dan <danielspadafora@gmail.com>
2021-10-04 12:28:36 -0300

    init

--- src/BaseSDK.js
+++ src/BaseSDK.js
@@ -12,7 +12,7 @@ const 	axios 	= require('axios'),
 
 module.exports = class BaseSDK {
 
-	#config:
+	#config;
 
 
 	constructor ( config ) {
@@ -40,12 +40,16 @@ module.exports = class BaseSDK {
 		{
 			throw new Error('trackEvent param must be a function');
 		}
-		else if ( 
-			config.auth 
-			&& !( config.auth instanceof AuthSDK )
-		)
-		{
-			throw new Error('auth param must be an instance of AuthSDK from the following package: auth-sdk-js');
+		else if ( config.auth ) {
+
+			if ( !( config.auth instanceof AuthSDK ) ) {
+
+				throw new Error('auth param must be an instance of AuthSDK from the following package: auth-sdk-js');	
+			}			
+		}
+		else {
+
+			config.auth = new AuthSDK( config );
 		}
 
 		this.#config = config;
@@ -60,7 +64,7 @@ module.exports = class BaseSDK {
 			headers: params.headers,
 			params: params.query,
 			data: params.data,
-			eventID: params.eventID,
+			eventId: params.eventId,
 			public: params.public,
 		});
 	}
@@ -74,7 +78,7 @@ module.exports = class BaseSDK {
 			headers: params.headers,
 			params: params.query,
 			maxContentLength: params.maxResponseSize ?? 2000,
-			eventID: params.eventID,
+			eventId: params.eventId,
 			public: params.public,
 		});
 	}
@@ -88,19 +92,19 @@ module.exports = class BaseSDK {
 			headers: params.headers,
 			params: params.query,
 			data: params.data,
-			eventID: params.eventID,
+			eventId: params.eventId,
 			public: params.public,
 		});
 	}
 
 
-	delete ( params ) {
+	remove ( params ) {
 
 		return this.#request({
 			url: params.path,
 			method: 'delete',
 			headers: params.headers,
-			eventID: params.eventID,
+			eventId: params.eventId,
 			public: params.public,
 		});
 	}
@@ -115,7 +119,7 @@ module.exports = class BaseSDK {
 			params: params.query,
 			data: params.data,
 			maxContentLength: params.maxResponseSize ?? 2000,
-			eventID: params.eventID,
+			eventId: params.eventId,
 			public: params.public,
 		});
 	}
@@ -172,9 +176,9 @@ module.exports = class BaseSDK {
 				params.headers = {};
 			}
 
-			params.baseURL: this.#config.baseURL;
-			params.responseType: 'json';
-			params.responseEncoding: 'utf8';		
+			params.baseURL = this.#config.baseURL;
+			params.responseType = 'json';
+			params.responseEncoding = 'utf8';		
 			params.headers['Content-Type'] = 'application/json';
 
 			if ( params.public ) {
@@ -209,8 +213,8 @@ module.exports = class BaseSDK {
 
 		return this.#prepare( params => {
 
-			let eventId = params.eventID;
-			delete params.eventID;
+			let eventId = params.eventId;
+			delete params.eventId;
 
 			return axios( params ).then( response => {
 
